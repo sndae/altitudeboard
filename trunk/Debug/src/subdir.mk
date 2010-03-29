03a766b253d5de73f57db732a9234e67b7bf8d7b
Anthon.Nilsson <Anthon.Nilsson@8e691a33-34d2-de65-796d-371292a91d8c>
2010-03-29 08:57:42 +0000

    Eclipse filer uppladdade

---
+++ trunk/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/i2c.c \
+../src/main.c \
+../src/temp.c \
+../src/uart.c 
+
+OBJS += \
+./src/i2c.o \
+./src/main.o \
+./src/temp.o \
+./src/uart.o 
+
+C_DEPS += \
+./src/i2c.d \
+./src/main.d \
+./src/temp.d \
+./src/uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=20000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
